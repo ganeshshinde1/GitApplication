f5404499e0df18c122fc932895f7c5f2b0048d5e
ganeshshinde1 <ganeshshinde2681998@gmail.com>
2021-09-15 06:54:06 -0700

    added changes for shinde

--- GitConcept/src/com/csi/encapsulationbase/Employee.java
+++ GitConcept/src/com/csi/encapsulationbase/Employee.java
@@ -1,5 +1,5 @@
 package com.csi.encapsulationbase;
-
+//ADDED BY GANESH SHINDE
 public class Employee {
 private int empId;
 private String empName;
@@ -18,7 +18,7 @@ public void setEmpName(String empName) {
 public static void main(String[] args) {
 	Employee e1 = new Employee();
 	e1.setEmpId(121);
-	e1.setEmpName("GANESH");
+	e1.setEmpName("GANESH SHINDE");
 	System.out.println("\n Employee Id:"+e1.getEmpId()+"\n Employee Name:"+e1.getEmpName());
 }
 }
